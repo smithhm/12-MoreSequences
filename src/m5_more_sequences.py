2151b7a0bbea17d6ec3ae23c8d2bfb5ac9cc965c
smithhm <smithhm@rose-hulman.edu>
2019-01-20 15:43:40 -0500

    M5 almost done

--- src/m5_more_sequences.py
+++ src/m5_more_sequences.py
@@ -8,15 +8,15 @@ for ITERATING through SEQUENCES, including:
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
          Mark Hays, Amanda Stouder, Aaron Wilkin, their colleagues,
-         and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         and Haiden Smith.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import rosegraphics as rg
 
 
 def main():
     """ Calls the   TEST   functions in this module. """
-    run_test_sum_radii()
+    # run_test_sum_radii()
     run_test_count_last_n_odds()
     run_test_index_of_first_negative()
     run_test_contains_an_a()
@@ -77,7 +77,7 @@ def sum_radii(circles):
       :rtype: int | float
     """
     # -------------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
+    # DONE: 2. Implement and test this function.
     #     The testing code is already written for you (above).
     #
     # Note: No fair using "slices" on ANY of these problems,
@@ -88,12 +88,17 @@ def sum_radii(circles):
     #
     #       Instead, use explicit loops, as you have for other problems.
     # -------------------------------------------------------------------------
-
+    total = 0
+    for k in range(len(circles)):
+        total = total + circles[k].radius
+    return total
 
 ###############################################################################
 # Some problems iterate (loop) through PART of the sequence,
 # perhaps BACKWARDS, as in the   count_last_n_odds   problem below.
 ###############################################################################
+
+
 def run_test_count_last_n_odds():
     """ Tests the   count_last_n_odds   function. """
     print()
@@ -152,9 +157,14 @@ def count_last_n_odds(integers, n):
       :rtype: int
     """
     # -------------------------------------------------------------------------
-    # TODO: 3. Implement and test this function.
+    # DONE: 3. Implement and test this function.
     #     The testing code is already written for you (above).
     # -------------------------------------------------------------------------
+    total = 0
+    for k in range(len(integers) - 1, len(integers) - 1 - n, -1):
+        if integers[k] % 2 == 1:
+            total = total + 1
+    return total
 
 
 ###############################################################################
